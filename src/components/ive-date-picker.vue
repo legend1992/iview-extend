d80959823fb0dd6722b81815588945ad77716453
lmlife2019 <287388482@qq.com>
2019-07-27 12:22:57 +0800

    feat: 增加日期选择框（单日期）

---
+++ src/components/ive-date-picker.vue
@@ -0,0 +1,57 @@
+<template>
+  <DatePicker :value="formatValue" @input="$emit('input', $event)" :type="type" :options="options" :placeholder="placeholder"></DatePicker>
+</template>
+
+<script>
+export default {
+  name: 'ive-date-picker',
+  props: {
+    value: {
+      type: [Date, String, Number],
+      default: null,
+    },
+    placeholder: {
+      type: String,
+      default: '请选择日期',
+    },
+    type: {
+      type: String,
+      default: 'date',
+    },
+    disabledDate: {
+      default: () => new Date(new Date() - 24 * 60 * 60 * 1000),
+    },
+  },
+  computed: {
+    formatValue() {
+      let formatValue = new Date(this.value);
+      if (!this.value || formatValue.toString() === 'Invalid Date') {
+        formatValue = null;
+      }
+      
+      return formatValue;
+    },
+    options() {
+      let disabledDateFn;
+      if (this.disabledDate instanceof Function) {
+        disabledDateFn = this.disabledDate;
+      } else {
+        disabledDateFn = (date) => {
+          let disabledDate;
+          if (date) {
+            const value = date && date.valueOf();
+            if (this.disabledDate && this.disabledDate instanceof Date) {
+              disabledDate = value < this.disabledDate;
+            }
+          }
+          
+          return disabledDate;
+        }
+      }
+      return {
+        disabledDate: disabledDateFn,
+      };
+    },
+  },
+};
+</script>
