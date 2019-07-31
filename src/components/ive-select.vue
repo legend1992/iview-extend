12464c65d67dfb38ec391d9d4d086834a020c7a7
lmlife2019 <287388482@qq.com>
2019-07-31 18:34:26 +0800

    feat: 增加parseIntKey属性

--- src/components/ive-select.vue
+++ src/components/ive-select.vue
@@ -11,7 +11,7 @@
     <Option
       v-for="(value, key) in formatOptions"
       :key="key"
-      :value="key"
+      :value="parseIntValue(value, key)"
     >{{value}}</Option>
   </Select>
 </template>
@@ -42,6 +42,15 @@ export default {
       type: String,
       default: '请选择',
     },
+    /**
+     * parseIntKey: 是否需要将key转换为整型
+     * 当将 id为1(整型), name为'名称' 的数据组合为 options: {1: '名称'} 时
+     * id由整型变成了对象options中的字符串，在组件中无法正确被选中
+    */
+    parseIntKey: {
+      type: Boolean,
+      default: false,
+    },
   },
   computed: {
     formatOptions() {
@@ -61,6 +70,15 @@ export default {
   methods: {
     queryChanged(e) {
       this.$emit('queryChanged', e);
+    },
+    parseIntValue(value, key) {
+      let newKey = key;
+      if (this.options instanceof Array) {
+        newKey = value;
+      } else if (this.parseIntKey) {
+        newKey = parseInt(key, 10);
+      }
+      return newKey;
     },
   },
 };
