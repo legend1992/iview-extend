caa48fe97cdaf608b2c83f94931e87eaa86b65f9
lmlife2019 <287388482@qq.com>
2019-08-01 10:36:14 +0800

    feat: 增加ive-checkbox-group组件

---
+++ src/components/ive-checkbox-group.vue
@@ -0,0 +1,21 @@
+<template>
+  <CheckboxGroup :value="value" @input="$emit('input', $event)">
+    <Checkbox v-for="(oValue, key) in options" :key="oValue" :label="key">{{ oValue }}</Checkbox>
+  </CheckboxGroup>
+</template>
+<script>
+export default {
+  name: 'ive-checkbox-group',
+  props: {
+    value: {
+      type: Array,
+      default: () => [],
+    },
+    options: {
+      type: [Array, Object],
+      default: () => ({}),
+    },
+  },
+}
+</script>
+
