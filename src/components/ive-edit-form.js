91e3c256dc8009736fb7d6296e72e606137f3270
liuyang0614 <yang.liu@imtpath.com>
2019-08-30 14:27:31 +0800

    fix: 修复批量新增后必填项的样式问题

--- src/components/ive-edit-form.js
+++ src/components/ive-edit-form.js
@@ -212,7 +212,7 @@ export default {
             tip ? renderTip(tip) : null,
           ];
         };
-        const { hide, label, prop, rules } = item;
+        const { hide, label, prop, rules, required } = item;
         return h(
           'FormItem',
           {
@@ -224,6 +224,7 @@ export default {
               label,
               prop,
               rules,
+              required,
             },
           },
           renderItem(item, type),
