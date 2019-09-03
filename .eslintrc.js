657681b89ce2d537cdaacc311b47138859071717
lmlife2019 <wx_55566cf164f24fcca76f7d5eec55fb04@git.code.tencent.com>
2019-09-03 18:56:40 +0800

    refactor: 完善eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'linebreak-style': ['off', 'windows'],
     'no-param-reassign': 0,
   },
   parser: 'vue-eslint-parser',
