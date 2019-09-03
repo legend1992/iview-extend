67546504c75b58f1e2f44fa306f0006f115aa627
lmlife2019 <wx_55566cf164f24fcca76f7d5eec55fb04@git.code.tencent.com>
2019-09-03 18:57:17 +0800

    refactor: 测试代码

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="ui-list">
-    <ive-filter-form
-      @query="query"
+    <ive-filter-form @query="query" :formConfig="formConfig" />
+    <!-- <ive-edit-form
       :formConfig="formConfig"
-    />
+    />-->
+    <ive-edit-modal id="1" :modal="true" :formConfig="formConfigBatchEdit" :getDetailApi="getDetailApi" />
   </div>
 </template>
 
@@ -15,7 +16,127 @@ export default {
         {
           prop: 'x1',
           label: 'x1',
+        }
+      ],
+      formConfigBatchEdit: [
+        {
+          prop: "startAt",
+          label: "开始时间",
+          itemConfig: {
+            tagName: "ive-date-picker",
+            props: {
+              type: "datetime"
+            },
+            on: {
+              // input: this.getStartAtVal
+            }
+          }
         },
+        {
+          prop: "endAt",
+          label: "结束时间",
+          itemConfig: {
+            tagName: "ive-date-picker",
+            props: {
+              type: "datetime",
+              disabledDate: null
+            }
+          }
+        },
+        {
+          prop: "mode",
+          label: "模式",
+          required: true,
+          itemConfig: {
+            tagName: "ive-select",
+            props: {
+              placeholder: "请选择模式",
+              // options: replyMode
+            }
+          }
+        },
+        {
+          prop: "scene",
+          label: "场景",
+          required: true,
+          itemConfig: {
+            tagName: "ive-select",
+            props: {
+              placeholder: "请选择场景",
+              // options: replyScene
+            },
+            on: {
+              // input: this.getSceneVal
+            }
+          }
+        },
+        {
+          prop: "detail",
+          label: "场景细项",
+          required: true,
+          itemConfig: {
+            tagName: "ive-select",
+            props: {
+              placeholder: "请选择场景细项",
+              options: []
+            }
+          }
+        },
+        {
+          prop: "active",
+          label: "是否启用",
+          required: true,
+          itemConfig: {
+            tagName: "ive-select",
+            value: 1,
+            props: {
+              placeholder: "是否启用",
+              // options: replyActive,
+              parseIntKey: true
+            }
+          }
+        },
+        {
+          prop: "text",
+          label: "文案",
+          required: true,
+          itemConfig: {
+            props: {
+              maxlength: 256
+            }
+          },
+          rules: [
+            {
+              // validator: this.$utils.strLengthCheck(31)
+            },
+            {
+              // validator: this.$utils.strRegularPattern()
+            }
+          ]
+        },
+        {
+          prop: "resourceType",
+          label: "资源类型",
+          itemConfig: {
+            tagName: "ive-select",
+            props: {
+              placeholder: "请选择资源类型",
+              // options: replyResourceType
+            },
+            on: {
+              // input: this.getResourceVal
+            }
+          }
+        },
+        {
+          prop: "resource",
+          label: "资源数据",
+          itemConfig: {
+            props: {
+              maxlength: 255
+            }
+          }
+        }
       ],
     };
   },
@@ -23,7 +144,16 @@ export default {
     query(e) {
       console.log(e);
     },
-  },
+    getDetailApi() {
+      return {
+        data: {
+          data: {
+            x1: "xxx"
+          }
+        }
+      };
+    }
+  }
 };
 </script>
 
