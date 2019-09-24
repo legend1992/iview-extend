119989549d36d2b2d6078e086e9476ae88044ce9
lmlife2019 <wx_55566cf164f24fcca76f7d5eec55fb04@git.code.tencent.com>
2019-09-24 18:55:13 +0800

    test: 完善ive-upload.vue测试

--- test/unit/ive-upload.spec.js
+++ test/unit/ive-upload.spec.js
@@ -5,23 +5,26 @@ import '../utils';
 import iveUpload from '../../src/components/ive-upload.vue';
 
 describe('ive-upload.vue', () => {
-  it('renders the correct markup', () => {
+  it('renders the correct markup & check props', () => {
+    const imgUrl = 'http://www.xxx.com/image.png';
     const wrapper = mount(iveUpload, {
       propsData: {
         action: '//jsonplaceholder.typicode.com/posts/',
+        value: imgUrl,
       },
     });
     expect(wrapper.classes()).to.include('ive-upload');
     expect(wrapper.find(Upload).exists()).to.equal(true);
+    // value & fileUrl
+    expect(wrapper.vm.fileUrl).to.equal(imgUrl);
   });
-  it('emit format-error', async () => {
+  it('check method: pickFile & checkFormat & checkResolutionRatio', async () => {
     const wrapper = mount(iveUpload, {
       propsData: {
         action: '//jsonplaceholder.typicode.com/posts/',
       },
     });
-    // format & emit format-error
-    const file = new File(['plain/text'], 'file');
+    const file = new File([], 'file');
     await wrapper.vm.pickFile(file);
     const emitted = wrapper.emitted();
     expect(emitted['format-error'].length).to.equal(1);
